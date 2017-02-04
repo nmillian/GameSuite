caee47bd10f398b3b9c00722cbc91a8ab0de0e21
nmillian <nmillian@ramapo.edu>
2017-02-04 18:00:14 -0500

    Connect 4 board

--- app/src/main/java/com/example/nicole/gamesuite/MainActivity.java
+++ app/src/main/java/com/example/nicole/gamesuite/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.nicole.gamesuite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //When the user clicks the Connect 4 button
+    public void connect4Click(View v){
+        Intent intent = new Intent(this, Connect4Activity.class);
+
+        startActivity(intent);
     }
 }

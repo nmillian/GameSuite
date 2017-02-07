2f73fc0cc7fb460c6e1da7edef904fe5079e0546
nmillian <nmillian@ramapo.edu>
2017-02-07 00:30:30 -0500

    Connect 4 board

--- app/src/main/java/com/example/nicole/gamesuite/Connect4Activity.java
+++ app/src/main/java/com/example/nicole/gamesuite/Connect4Activity.java
@@ -2,6 +2,8 @@ package com.example.nicole.gamesuite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Connect4Activity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class Connect4Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect4);
+    }
+
+    public void tileClick(View view){
+
+        //Get the id of the tile clicked
+        String tile = getResources().getResourceEntryName(view.getId());
+
+        //Get the tile row and column
+        String rowString = Character.toString(tile.charAt(4));
+        String columnString = Character.toString(tile.charAt(5));
+
+        view.setBackgroundResource(R.drawable.redcircle);
+
     }
 }
---
+++ app/src/main/java/com/example/nicole/gamesuite/Connect4Board.java
@@ -0,0 +1,8 @@
+package com.example.nicole.gamesuite;
+
+/**
+ * Created by Nicole on 2/7/2017.
+ */
+
+public class Connect4Board {
+}

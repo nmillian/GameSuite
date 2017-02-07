104ff06eb142e71654527987034e77cf7c86365a
nmillian <nmillian@ramapo.edu>
2017-02-07 02:18:55 -0500

    Connect 4 implementation

--- app/src/main/java/com/example/nicole/gamesuite/Connect4Activity.java
+++ app/src/main/java/com/example/nicole/gamesuite/Connect4Activity.java
@@ -7,12 +7,32 @@ import android.widget.ImageButton;
 
 public class Connect4Activity extends AppCompatActivity {
 
+    /* *********************************************
+`   * Private class variables and objects
+********************************************* */
+    //The Connect 4 board object
+    private Connect4Board board;
+
+    //The current player
+    private String currentPlayer = "H";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect4);
     }
 
+    /* *********************************************
+`   * Constructor
+    ********************************************* */
+    public Connect4Activity(){
+        board = new Connect4Board();
+    }
+
+    /* *********************************************
+`   * Public functions
+    ********************************************* */
     public void tileClick(View view){
 
         //Get the id of the tile clicked
@@ -22,7 +42,27 @@ public class Connect4Activity extends AppCompatActivity {
         String rowString = Character.toString(tile.charAt(4));
         String columnString = Character.toString(tile.charAt(5));
 
-        view.setBackgroundResource(R.drawable.redcircle);
+        if(currentPlayer.equals("H")){
+            if(board.choiceValid(rowString, columnString)){
+                board.updateHumanMove(rowString, columnString);
+                view.setBackgroundResource(R.drawable.redcircle);
+                currentPlayer = "C";
+            }
+        }
+
+        else{
+            if(board.choiceValid(rowString, columnString)){
+                board.updateComputerMove(rowString, columnString);
+                view.setBackgroundResource(R.drawable.yellowcircle);
+                currentPlayer = "H";
+            }
+        }
+    }
+
+    /* *********************************************
+`   * Private functions
+    ********************************************* */
+    private void playGame(){
 
     }
 }
--- app/src/main/java/com/example/nicole/gamesuite/Connect4Board.java
+++ app/src/main/java/com/example/nicole/gamesuite/Connect4Board.java
@@ -1,8 +1,80 @@
 package com.example.nicole.gamesuite;
 
+import android.util.Log;
+
+import java.util.Hashtable;
+
 /**
  * Created by Nicole on 2/7/2017.
  */
 
 public class Connect4Board {
+    /* *********************************************
+`   * Private class variables
+    ********************************************* */
+    //Hashtable to hold the the type of piece located at each space
+    private static Hashtable<String, String> connect4Board = new Hashtable<String, String>();
+
+    /* *********************************************
+ `   * Constructor
+     ********************************************* */
+    public Connect4Board(){
+        //Initialize the Hashtable with the defualt values at default locations
+        connect4Board.clear();
+        initializeBoard();
+    }
+
+    /* *********************************************
+`   * Private class functions
+    ********************************************* */
+    private void initializeBoard(){
+        String row;
+        String column;
+        String tile;
+
+        //Row
+        for(int i = 1; i < 7; i++ ){
+            //Column
+            for(int j = 1; j < 8; j++){
+                row = String.valueOf(i);
+                column = String.valueOf(j);
+
+                tile = row + column;
+
+                Log.d("TILE", tile);
+
+                //All the tiles are originally blank
+                connect4Board.put(tile, "B");
+
+                Log.d("BOARD", connect4Board.get(tile));
+            }
+        }
+    }
+
+    /* *********************************************
+`   * Public class variables
+    ********************************************* */
+    public boolean choiceValid(String row, String column){
+        String tile = row + column;
+        Log.d("VALID", connect4Board.get(tile));
+
+        if(connect4Board.get(tile).equals("B")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void updateHumanMove(String row, String column){
+        String tile = row + column;
+        connect4Board.put(tile, "H");
+    }
+
+    public void updateComputerMove(String row, String column){
+        String tile = row + column;
+        connect4Board.put(tile, "C");
+    }
+
+
 }
